d9a7f9ae02e0f5a3f68b5255e8fa0be11c03d8a8
Sinclair81 <sinclair81@hotmail.de>
2016-08-21 11:25:53 +0200

    add setStatus() for Status Updats

--- examples/WriteMultipleVoltages/WriteMultipleVoltages.ino
+++ examples/WriteMultipleVoltages/WriteMultipleVoltages.ino
@@ -121,6 +121,8 @@ void loop() {
     pinVoltage = analogRead(A7) * (VOLTAGE_MAX / VOLTAGE_MAXCOUNTS);
     ThingSpeak.setField(8,pinVoltage);
   #endif
+  
+  ThingSpeak.setStatus("New Values!");
 
   // Write the fields that you've set all at once.
   ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);  
