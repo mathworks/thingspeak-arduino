337b27bdc679be9ece7c6689a2a835548f90dd4c
Martin <martinius96@gmail.com>
2018-11-10 23:49:39 +0100

    Create secrets.h

---
+++ examples/ESP32/ReadFieldEnterprise/secrets.h
@@ -0,0 +1,4 @@
+#define SECRET_CH_ID_WEATHER_STATION 12397	          	//MathWorks weather station
+
+#define SECRET_CH_ID_COUNTER 298725 					//Test channel for counting
+#define SECRET_READ_APIKEY_COUNTER "SODG0O2UZVGKWAWG"	//API Key for Test channel
